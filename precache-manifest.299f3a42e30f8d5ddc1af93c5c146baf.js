06ed10037925114e1ccd031006cfecb0741c8ad7
h2ash <hun22ash@gmail.com>
2019-07-02 18:54:48 +0300

    Updates

---
+++ precache-manifest.299f3a42e30f8d5ddc1af93c5c146baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9079d20c0742b71b0c3d67c31fa15351",
+    "url": "/react_static-list-of-todos2/index.html"
+  },
+  {
+    "revision": "d9b44e7b81894430a73d",
+    "url": "/react_static-list-of-todos2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_static-list-of-todos2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d9b44e7b81894430a73d",
+    "url": "/react_static-list-of-todos2/static/js/main.6dbf3635.chunk.js"
+  },
+  {
+    "revision": "66d2cdaf83432e83a8e5",
+    "url": "/react_static-list-of-todos2/static/js/runtime~main.63a2f05f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_static-list-of-todos2/static/media/logo.5d5d9eef.svg"
+  }
+]);
